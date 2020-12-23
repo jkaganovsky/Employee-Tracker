3dce8ae53f38f40a894e3099411323fef5807cbe
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-12-22 19:12:58 -0800

    Added mysql code file in seed.sql.

---
+++ seed.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM employee_role;
+SELECT * FROM employee;
+
