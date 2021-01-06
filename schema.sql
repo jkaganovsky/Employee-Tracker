e3f614d6144fa85270d3243c8a971e4309a7b1fe
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-05 21:33:29 -0800

    Added manager_id and changed decimal to schema.

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE employee_role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NULL,
-  salary DECIMAL(10,2) NULL,
+  salary DECIMAL(12,2) NULL,
   department_id INT NULL,
   PRIMARY KEY (id)
 );
@@ -23,6 +23,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
+  manager_id INT,
   PRIMARY KEY (id)
 );
 
